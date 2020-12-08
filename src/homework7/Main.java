caf17e756dbe907b2db3a840bbeaa4805ce2d8ee
crangoren <crangoren@gmail.com>
2020-12-08 22:17:55 +0300

    added hw7

--- src/homework7/Main.java
+++ src/homework7/Main.java
@@ -1,9 +1,46 @@
-
 package homework7;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+//        Cat cat = new Cat("Murzik1", 10);
+//        Plate plate = new Plate(100);
+        Plate plate = new Plate(70);
+
+        Cat[] cats = {
+                new Cat("Barsik", 20),
+                new Cat("Pushok", 30),
+                new Cat("Murzik", 35),
+                new Cat("Kisya", 30),
+                new Cat("Hvostik", 10),
+
+        };
+        plate.info();
+//        cat.eat(plate);
+//        cat.getHungry();
+//        plate1.getFood();
+//        plate.info();
+
+        for (int i = 0; i < cats.length; i++) {
+
+            cats[i].eat(plate);
+
+//            cats[i].getHungry(); // задание 4
+
+            plate.info();
+
+        }
+        for (int j =0; j < cats.length; j++) {
+
+            if (cats[j].getHungry()) {
+                System.out.println(cats[j].getName() + " поел");
+            } else {
+                System.out.println(cats[j].getName() + " голодный");
+            }
+        }
+
+
+
     }
 }
--- src/homework7/Plate.java
+++ src/homework7/Plate.java
@@ -2,4 +2,35 @@ package homework7;
 
 public class Plate {
 
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public  void decreaseFood(int amount) {
+        if (amount < 0 || amount > this.getFood()) {
+            return;
+        } else {
+            food -= amount;
+        }
+    }
+
+    public void info() {
+        System.out.println("Еды в тарелке: " + food + "\n");
+    }
+
+//    public void addFood(int value) { // метод задания 6
+//        if (value > food) {
+//            int feed = (value - food);
+//            food += feed;
+//            System.out.println("Добавлено еды: " + (feed));
+//        } else {
+//            return;
+//        }
+//    }
 }
